59f93d3bacf6c8aa0dbe92288db36e32ae253b01
Greg Albrecht <gba@splunk.com>
2012-05-30 11:37:27 -0700

    added room support and a bit of cleanup before rearranging the namespace.

--- campfire_handler.gemspec
+++ campfire_handler.gemspec
@@ -1,26 +1,17 @@
 # -*- encoding: utf-8 -*-
-$:.push File.expand_path("../lib", __FILE__)
-require "campfire_handler"
+$:.push File.expand_path('../lib', __FILE__)
+require 'chef/handler'
 
 Gem::Specification.new do |s|
-  s.name        = "campfire_handler"
-  s.version     = CampfireHandler::VERSION
-  s.platform    = Gem::Platform::RUBY
-  s.authors     = ["Brian Scott"]
-  s.email       = ["brainscott@gmail.com"]
-  s.homepage    = "https://github.com/bscott/campfire_handler"
-  s.summary     = %q{Chef handler for sending exceptions to Campfire}
-  s.description = %q{Chef handler for sending exceptions to Campfire}
-
-  s.rubyforge_project = "campfire_handler"
-
-  s.files         = `git ls-files`.split("\n")
-  s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
-  s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
-  s.require_paths = ["lib"]
-
-  s.add_dependency("chef", ">= 0.9.0")
-  s.add_dependency("tinder")
-
-  s.add_development_dependency("mocha")
+  s.name = 'chef-handler-campfire'
+  s.version = Chef::Handler::Campfire::VERSION
+  s.platform = Gem::Platform::RUBY
+  s.authors = ['Brian Scott', 'Greg Albrecht']
+  s.email = ['brainscott@gmail.com', 'gba@splunk.com']
+  s.homepage = 'https://github.com/ampledata/chef-handler-campfire'
+  s.summary = 'Chef Exception & Reporting Handler for Campfire'
+  s.description = 'Allows reporting of Chef Exceptions to Campfire Chat Rooms.'
+  s.files = %w(LICENSE README.rdoc) + Dir.glob('lib/**/*')
+  s.require_paths = ['lib']
+  s.add_runtime_dependency('tinder')
 end
--- lib/campfire_handler.rb
+++ lib/campfire_handler.rb
@@ -1,19 +1,45 @@
-require "chef/handler"
+#!/usr/bin/env ruby
+# Chef Exception & Reporting Handler for Campfire.
+#
+# Author:: Greg Albrecht <mailto:gba@splunk.com>
+# Author:: Brian Scott <mailto:brainscott@gmail.com>
+# Copyright:: Copyright 2012 Splunk, Inc.
+# License:: Apache License 2.0
+#
+
+
+require 'rubygems'
+require 'chef/handler'
 require 'tinder'
 
-class CampfireHandler < Chef::Handler
-  VERSION = "0.0.1"
-  
-  def initialize(opts = {})
-    @config = opts
-  end
 
-  def report()
-    if run_status.failed?
-      Chef::Log.error("Creating Campfire exception report")
-      campfire = Tinder::Campfire.new(@config[:subdomain], :token => @config[:token])
-      campfire.rooms.first.speak("#{node.hostname} #{run_status.formatted_exception}")
-      campfire.rooms.first.paste(Array(backtrace).join("\n"))
+
+class Chef
+  class Handler
+    class Campfire < Chef::Handler
+      VERSION = '2.0.0'
+
+      def initialize(subdomain, token, room)
+        @subdomain = subdomain
+        @token = token
+        @room = room
+      end
+
+      def report
+        if run_status.failed?
+          Chef::Log.error('Creating Campfire exception report.')
+          campfire = Tinder::Campfire.new(@subdomain, :token => @token)
+
+          room = if @room.nil?
+                   campfire.rooms.first
+                 else
+                   campfire.find_room_by_id(@room.to_i)
+                 end
+
+          room.speak([node.hostname, run_status.formatted_exception].join(' '))
+          room.paste(Array(backtrace).join('\n'))
+        end
+      end
+    end
   end
-end
 end
